f54039da5a0f132c953742216ced95ce1e861214
Nawaraj1111 <53786438+Nawaraj1111@users.noreply.github.com>
2020-02-03 19:32:59 +0545

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld
 {
   public static void main(String []args)
   {
+    //This is printing statement in java
     System.out.println("Hello world");
     System.out.println("Hello world1");
     System.out.println("Hello world2");
